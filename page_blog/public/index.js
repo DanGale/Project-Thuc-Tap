24f54f6bb9ddab0caecbd50c6e8ad7fc8e1efabe
Fest <100851969+NguyenHoi@users.noreply.github.com>
2023-12-12 17:25:48 +0700

    HOi commit

--- page_blog/public/index.js
+++ page_blog/public/index.js
@@ -26,3 +26,16 @@ document.addEventListener('click', function (event) {
 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('.nav a');
+
+    links.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        links.forEach(function(otherLink) {
+          otherLink.classList.remove('active');
+        });
+
+        link.classList.add('active');
+      });
+    });
+  });
